d783f8631ed9973df270afcb940472348c6e1ced
Ekku Leivonen <ekku.leivonen@gmail.com>
2026-03-03 12:22:21 +0900

    ...

--- backend/settings.py
+++ backend/settings.py
@@ -1,15 +1,8 @@
 from _utils.environ import env
 
-
-
 # =============================================================================
 # GitHub
 # =============================================================================
-
-GITHUB_TOKEN: str = env.str("GITHUB_TOKEN", required=True)
-GITHUB_OWNER: str = env.str("GITHUB_OWNER", required=True)
-GITHUB_REPO: str = env.str("GITHUB_REPO", required=True)
-GITHUB_DEFAULT_BRANCH: str = env.str("GITHUB_DEFAULT_BRANCH", default="main")
 
 
 # =============================================================================
