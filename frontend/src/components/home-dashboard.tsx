796ddac799be56e5d2ebe0049d7042c20072a9c9
Ekku Leivonen <ekku.leivonen@gmail.com>
2026-03-08 00:10:02 +0900

    ...

--- frontend/src/components/home-dashboard.tsx
+++ frontend/src/components/home-dashboard.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Folder, Layout, Layers, Server, Zap } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useNodesStore } from "@/stores/nodes";
 import { useRackStore } from "@/stores/racks";
